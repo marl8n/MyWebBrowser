78c1007df4d457757692ad88853365a335e76611
marl8n <57814291+marl8n@users.noreply.github.com>
2021-02-12 11:16:03 -0600

    Updated UrlUtils
    better regex

--- utils/UrlUtils.cs
+++ utils/UrlUtils.cs
@@ -12,9 +12,8 @@ namespace MyWebBrowser.utils
         public static string NormalizeUrl(string url)
         {
             url.Trim();
-            // ^(https)?(http)?://w{3}?\.?\w+\.?\w{3}.*$
-            // ^(https)?(http)?://w{3}?\.?\\w+\.?\\w{3}.*$
-            Regex completeUrl = new Regex("^(https)?(http)?://w{3}?\\.?\\w+\\.?\\w{3}.*$");
+            // ^(https)?(http)?(://)?\w{3}?\.?\w+\.?\w{3,5}.*$
+            Regex completeUrl = new Regex("^(https)?(http)?(://)?\\w{3}?\\.?\\w+\\.?\\w{3,5}.*$");
 
             if ( !completeUrl.IsMatch(url) )
             {
