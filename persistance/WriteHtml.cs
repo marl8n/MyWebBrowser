071f9df6f8b1cad47ada6afa19aba4ba08f56eaa
marl8n <57814291+marl8n@users.noreply.github.com>
2021-02-18 18:14:41 -0600

    Update WriteHtml.cs
    Added styles in html

--- persistance/WriteHtml.cs
+++ persistance/WriteHtml.cs
@@ -15,7 +15,13 @@ namespace MyWebBrowser.persistance
 <head>
     <meta charset=""UTF-8"" />
     <meta name=""viewport"" content=""width=device-width, initial-scale=1.0""
-    <link rel=""stylesheet"" href=""./styles.css"">
+    <style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    </style>
     <title>History</title>
 </head>
 ";
